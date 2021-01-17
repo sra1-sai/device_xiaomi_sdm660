f6f4e1c66287ffc8ea76bd4d97e1b20bf11991ea
Adithya R <gh0strider.2k18.reborn@gmail.com>
2021-01-17 22:29:22 +0530

    sdm660-common : Enable QCRIL radio power saving

--- properties.mk
+++ properties.mk
@@ -173,6 +173,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.radio.multisim.config=dsds \
     persist.vendor.data.iwlan.enable=true \
     persist.vendor.data.mode=concurrent \
+    persist.vendor.radio.add_power_save=1 \
     persist.vendor.radio.apm_sim_not_pwdn=1 \
     persist.vendor.radio.atfwd.start=true \
     persist.vendor.radio.custom_ecc=1 \
