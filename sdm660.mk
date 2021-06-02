c4edc77d646a442598ae46bc351200afe7d69514
SALMANKHANPM <patanmohammadsalmankhan@gmail.com>
2021-06-03 02:20:26 +0530

    sdm660-common : 11~ifY !!!!!

--- sdm660.mk
+++ sdm660.mk
@@ -28,7 +28,7 @@ PRODUCT_COMPATIBLE_PROPERTY_OVERRIDE := true
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-11
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
