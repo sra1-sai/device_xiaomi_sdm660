fb742ff3fe9b67490076a3826be39b73829d8cc9
SALMANKHANPM <patanmohammadsalmankhan@gmail.com>
2021-06-11 20:14:18 +0530

    sdm660-common : Track & Build With ProtonClang 13.0.0

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -7,3 +7,7 @@ rm -rf hardware/qcom-caf/msm8998/display/
 # Display Hal Required To Get Working WFD
 
 git clone https://github.com/SALMANKHANPM/android_hardware_qcom_display.git hardware/qcom-caf/msm8998/display
+
+# Clone ProtonClang-13
+
+git clone --depth=1 https://github.com/kdrag0n/proton-clang prebuilts/clang/host/linux-x86/clang-proton
